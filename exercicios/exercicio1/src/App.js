ec099e18fe5beaf77feefc128b1cb881283b686f
Leonardo Marcatti da Silva <leonardomarcatti@hotmail.com>
2023-02-19 17:06:10 -0300

    Melhorias

--- exercicios/exercicio1/src/App.js
+++ exercicios/exercicio1/src/App.js
@@ -1,26 +1,33 @@
 import './App.css';
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import Area from './components/Area';
 import Button from './components/Button';
 
 function App() {
   let [balls, setBalls] = useState([]);
-  const [clicked, setClicked] = useState(false);
+  let [backupBalls, setBackup] = useState([]);
 
   const handleBall = e => {
     const {clientX, clientY} = e;
     setBalls((prevState) => [...prevState, {clientX, clientY}]);
   }
 
-  const handleItens = () => {
+  const clearLast = () => {
     const length = balls.length;
+    setBackup(prevState => [...prevState, balls[length-1]])
+    
     const myArray = balls.filter((el, i) => i < length);
     myArray.pop();
+    setBalls(myArray);
     return myArray;
   }
 
-  const handleClick = () => {
-    setClicked((prevState) => !prevState);
+  const returnLast = () => {
+    const last = (backupBalls.slice(-1).pop());
+    if (backupBalls.slice(-1).pop()) {
+      setBalls(prevState => [...prevState, last])
+      backupBalls.pop()
+    }
   }
 
   return (
@@ -29,12 +36,12 @@ function App() {
         <div></div>
         <h1>Balões!</h1>
         <div>
-          <Button className="btn btn-danger" value="Limpar Último" onClick={handleClick} disabled={clicked}/>
-          <Button className="btn btn-success" value="Refazer" onClick={handleClick} disabled={!clicked}/>
+          <Button className="btn btn-danger" value="Limpar Último" onClick={clearLast}/>
+          <Button className="btn btn-success" value="Refazer" onClick={returnLast}/>
         </div>
       </header>
       <Area
-        itens={(clicked)? handleItens() : balls}
+        itens={balls}
         onAddBall={handleBall}
       />
     </div>
