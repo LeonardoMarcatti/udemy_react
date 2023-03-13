7f49adacb939d6fea09f0015eb1a2b321363aa25
Leonardo Marcatti da Silva <leonardomarcatti@hotmail.com>
2023-03-13 10:41:55 -0300

    ok

--- secao13/src/components/User.js
+++ secao13/src/components/User.js
@@ -1,5 +1,11 @@
 import classes from './User.module.css';
 
+class User {
+  render(){
+    
+  }
+}
+
 const User = props => {
   const {name} = props
   return <li className={classes.user}>{name}</li>;
